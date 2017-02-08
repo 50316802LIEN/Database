4ea860d6a5f8f8adfba786ba53214245c1062e96
50316802LIEN <lien.nguyen0418@gmail.com>
2017-02-09 00:11:41 +0900

    Create GraphDB_main.java

---
+++ GraphDB_main.java
@@ -0,0 +1,20 @@
+package graphdb;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GraphDB_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GraphDB_view graph = new GraphDB_view();
+		graph.setBounds(5,5,655,455);
+		graph.setVisible(true);
+	
+		
+		
+			
+		}
+	}
+              
